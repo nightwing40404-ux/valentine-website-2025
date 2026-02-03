cd4b8c608344f6769e20db63ab639ca96039418e
nightwing40404-ux <nightwing40404@gmail.com>
2026-02-03 10:09:35 +0800

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Jade",
+    valentineName: "Snehaa",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
@@ -25,10 +25,10 @@ const CONFIG = {
             text: "Do you like me?",                                    // First interaction
             yesBtn: "Yes",                                             // Text for "Yes" button
             noBtn: "No",                                               // Text for "No" button
-            secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
+            secretAnswer: "I Thought i liked you... turns out its much more than that! ❤️"           // Secret hover message
         },
         second: {
-            text: "How much do you love me?",                          // For the love meter
+            text: "How much of your hearth do i have?",                          // For the love meter
             startText: "This much!",                                   // Text before the percentage
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
@@ -49,8 +49,8 @@ const CONFIG = {
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        title: "Yayyyyyyyyy! Guess Who’s Smiling rn? hehe 🎉💝💖💝💓",
+        message: "Be ready on February 14th. I’ll come pick you up... don’t be late!",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
