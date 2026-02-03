ba30c7bb3c50e6524322cdfa16099ce46a9a795a
nightwing40404-ux <nightwing40404@gmail.com>
2026-02-03 10:18:49 +0800

    Update config.js

--- config.js
+++ config.js
@@ -25,15 +25,15 @@ const CONFIG = {
             text: "Do you like me?",                                    // First interaction
             yesBtn: "Yes",                                             // Text for "Yes" button
             noBtn: "No",                                               // Text for "No" button
-            secretAnswer: "I Thought i liked you... turns out its much more than that! ❤️"           // Secret hover message
+            secretAnswer: "Let’s just say my heart already made up its mind! ❤️"           // Secret hover message
         },
         second: {
-            text: "How much of your hearth do i have?",                          // For the love meter
+            text: "How much of my heart is yours?",                          // For the love meter
             startText: "This much!",                                   // Text before the percentage
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2025? 🌹", // The big question!
+            text: "Will you be my Valentine on February 14th, 2026? 🌹", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
@@ -42,14 +42,14 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "WOOOOW are you trying to steal my whole heart?? 🥰🚀💝",  // Shows when they go past 5000%
+        high: "Someone’s heart just overflowed! 🚀💝",              // Shows when they go past 1000%
+        normal: "It’s getting sweeter! 🥰"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yayyyyyyyyy! Guess Who’s Smiling rn? hehe 🎉💝💖💝💓",
+        title: "Yayyyyyyyyy! Guess Who’s Smiling like an idiot? 🎉💝💖💝💓",
         message: "Be ready on February 14th. I’ll come pick you up... don’t be late!",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
